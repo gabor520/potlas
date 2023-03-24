aff27df790b7bd9f992447eb15d5d940a541c513
gabor520 <pinielgabor1@gmail.com>
2023-03-24 11:03:44 +0100

    haladas

---
+++ main.js
@@ -0,0 +1,36 @@
+<button onclick="tomb">Click me</button>
+var tomb=[];
+
+  
+
+for (let index = 0; index < 50; index++) {
+let x= Math.floor(Math.random() * 100); 
+    tomb.push(x)
+}  
+console.log('tomb')
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+
+
+
+
+
+
+
+
+for (let index = 0; index %2==index; index++) {
+let x
+    
+}
+console.log("párosszámok");
+
+
+
+
+
